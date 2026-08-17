b3d39996cfcfe8e865243174cce9c49e99fa2649
sofiacpaiva <sofiacavalcantedepaiva@gmail.com>
2026-08-17 14:05:03 -0300

    att

--- myproject/posts/views.py
+++ myproject/posts/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
 
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    posts = Post.objects.all().order_by('-date')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
+
+def post_page(request, slug):
+    return HttpResponse(slug)
